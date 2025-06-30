4866682707cdca75081698af34663335bd17f632
Octopus136 <1361227324@qq.com>
2025-06-30 16:54:12 +0800

    Update Interfaces

--- calculate.py
+++ calculate.py
@@ -3,7 +3,7 @@ from thop import profile
 import torch
 from thop import clever_format
 input1 = torch.randn(1, 3, 480, 640) 
-model = YOLO('./yolov8n.pt')
+model = YOLO('yolo11.pt')
 macs, params = profile(model.model, inputs=(input1, ))
 macs, params = clever_format([macs, params], "%.3f")
 print('macs :',macs, 'params:',params)
--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
-model = YOLO('./yolov8m.pt')
+model = YOLO('yolov11.pt')
 model.export(format='rknn')
