1b56a3f6d7b6cbbc53c31be6aedff03bddd5d958
翔宇 战 <1361227324@qq.com>
2025-07-03 16:04:28 +0800

    Add sparse_training params

--- prune.py
+++ prune.py
@@ -7,7 +7,7 @@ model = YOLO('yolo11.yaml')
 
 def prunetrain(train_epochs, prune_epochs=0, quick_pruning=True, prune_ratio=0.5, 
                prune_iterative_steps=1, data='coco.yaml', name='yolo11', imgsz=640, 
-               batch=8, device=[0]):
+               batch=8, device=[0], sparse_training=False):
     if not quick_pruning:
         assert train_epochs > 0 and prune_epochs > 0, "Quick Pruning is not set. prune epochs must > 0."
         model.train(data=data, epochs=train_epochs, imgsz=imgsz, batch=batch, device=device, name=name, prune=False)
@@ -27,7 +27,8 @@ prunetrain(quick_pruning=False,       # Quick Pruning or not
            device=[0],                # GPU devices
            name='yolo11',             # Save name
            prune_ratio=0.5,           # Pruning Ratio (50%)
-           prune_iterative_steps=1)   # Pruning Interative Steps
+           prune_iterative_steps=1,   # Pruning Interative Steps
+           sparse_training=True)      # Experimental, Allow Sparse Training Before Pruning
 
 # Quick Pruning (prune_epochs no need)
 # prunetrain(quick_pruning=True, data='coco.yaml', train_epochs=10, imgsz=640, batch=8, device=[0], name='yolo11', 
